4241446ed26b88a8876792abb276be94caec3709
SaltwaterC <saltwaterc@gmail.com>
2013-01-19 15:02:09 +0200

    Updated the year range for the LICENSE.md. Tested from node v0.4.9-v0.8.18. Better jslint validation for the common test file. Travis CI integration.

--- tests/includes/test.js
+++ tests/includes/test.js
@@ -1,13 +1,13 @@
+'use strict';
+
+var mime = require('../../');
+var assert = require('assert');
+
 // This is the generic test for:
 // path/to/file => mime/type
 // ['path/to/file1', 'path/to/file2'] => ['mime/type1', 'mime/type2']
 
 module.exports = function (path, expectedMime) {
-	'use strict';
-	
-	var mime = require('../../');
-	var assert = require('assert');
-	
 	var callback = 0;
 	
 	mime(path, function (err, res) {
